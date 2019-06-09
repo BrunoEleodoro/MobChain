c4e0727d85ca56d764ea803defc114d923fe12a0
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-06-09 04:05:30 -0300

    Login starts the redesign

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,57 +9,63 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Login'),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: <Widget>[
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          'Login',
-                          style: TextStyle(fontSize: 25),
+      body: SingleChildScrollView(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: double.maxFinite,
+                  height: MediaQuery.of(context).size.height - 50,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        height: 300,
+                        width: 300,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 100),
+                        width: 300,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                            Color(0XFF15CCEA),
+                            Color(0XFF65D7A0),
+                          ]),
+                          borderRadius: BorderRadius.all(Radius.circular(3))
                         ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                              hintText: 'Email', border: OutlineInputBorder()),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                              hintText: 'Senha', border: OutlineInputBorder()),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          width: double.maxFinite,
-                          child: RaisedButton.icon(
-                              onPressed: () {
-                                Navigator.pushReplacementNamed(context, '/home');
-                              },
-                              icon: Icon(Icons.navigate_next),
-                              label: Text('FAZER LOGIN')),
-                        )
-                      ],
-                    ),
+                        child: Center(child: Text('Entrar', style: TextStyle(color: Colors.white),)),
+                      ),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Não tem uma conta?',
+                            style: TextStyle(color: Colors.black54),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text('Cadastre-se!',
+                              style: TextStyle(
+                                color: Color(0XFFFA6423),
+                              ))
+                        ],
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-        ));
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
